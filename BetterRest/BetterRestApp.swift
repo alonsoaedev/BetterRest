4c548d821a78ee37649fd8a29158c86733ba492c
Alonso Acosta <alonso.ae@hotmail.com>
2025-06-10 08:04:01 -0600

    first commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alonso Acosta Enriquez on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Alonso Acosta Enriquez on 07/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
